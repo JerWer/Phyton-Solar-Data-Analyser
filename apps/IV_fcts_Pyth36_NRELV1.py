f7dd8d36f64a9e1ead5f9508a3032fad3a3e549b
jwerner <jeremie.werner@hotmail.com>
2019-06-10 21:49:24 -0600

    ...

--- apps/IV_fcts_Pyth36_NRELV1.py
+++ apps/IV_fcts_Pyth36_NRELV1.py
@@ -83,10 +83,6 @@ add time plot to autoanalysis if span over 5hrs
 
 - add reminder to save session when quit the window
 
-- compatibility with cigs station: check if current in itx files is current or current density
-problem with forward/reverse, problem with excel file
-problem of double files?
-
 
 """
 #%%############# Global variable definition
